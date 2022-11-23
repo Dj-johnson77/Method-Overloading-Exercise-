4cd19e905cc7fe4b2fa5be830213454d66853dfc
Donald Johnson <donaldrjohnson66@gmail.com>
2022-11-23 17:03:39 -0600

    Finished

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,5 +6,33 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool c)
+        {
+            if (c && (a + b) != 1)
+            {
+                return $"{a + b} dollars";
+            }
+            else if (c && (a + b) == 1)
+            {
+                return $"{a + b} dollar";
+            }
+            else
+            {
+                return $"{a + b}";
+            }
+        }
+    
+        
+        
+            
+        
     }
 }
